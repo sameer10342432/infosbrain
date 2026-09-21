a6c628988f5283b0956981250a75424d481c2a7c
sameer10342432 <sameer10342432@users.noreply.github.com>
2026-09-21 07:48:57 -0700

    fix: SPA routing support for GitHub Pages

--- scripts/copy-404.js
+++ scripts/copy-404.js
@@ -1,10 +1,32 @@
 import fs from 'fs';
 import path from 'path';
 
-const distIndex = path.resolve(process.cwd(), 'dist', 'index.html');
-const dist404 = path.resolve(process.cwd(), 'dist', '404.html');
+const distDir = path.resolve(process.cwd(), 'dist');
+const dist404 = path.resolve(distDir, '404.html');
 
-if (fs.existsSync(distIndex)) {
-  fs.copyFileSync(distIndex, dist404);
-  console.log('[Build] Successfully generated dist/404.html for SPA static hosting');
+const redirectHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>InfosBrain | Loading...</title>
+    <script type="text/javascript">
+      var pathSegmentsToKeep = 1;
+      var l = window.location;
+      l.replace(
+        l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+        l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+        l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+        (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+        l.hash
+      );
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
+`;
+
+if (fs.existsSync(distDir)) {
+  fs.writeFileSync(dist404, redirectHtml, 'utf8');
+  console.log('[Build] Successfully generated dist/404.html with SPA redirect for GitHub Pages');
 }
